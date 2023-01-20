0020d3b8229699afa25e06041e4a5c2371c592df
Andrew <33009904+ColdDragon@users.noreply.github.com>
2023-01-20 10:52:53 +0900

    Delete libpng_sl.sh

--- libpng_sl.sh
+++
@@ -1,3 +0,0 @@
-cd libpng
-ar   -r libPOlibpng.a png.o pngerror.o pngget.o pngmem.o pngpread.o pngread.o pngrio.o pngrtran.o pngrutil.o pngset.o pngtrans.o pngwio.o pngwrite.o pngwtran.o pngwutil.o
-cd ..
